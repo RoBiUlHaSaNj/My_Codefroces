d215f513acaadfa81bb5bf97b4c5b44f73e4a058
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-09 23:48:42 +0600

    B - New Year's Number.cpp

---
+++ B - New Year's Number.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+	int t;
+	cin>>t;
+	int year;
+
+	while(t--){
+
+
+
+        cin>>year;
+
+
+         if (year % 2020 <= year / 2020) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+
+
+
+
+
+	}
+
+	}
