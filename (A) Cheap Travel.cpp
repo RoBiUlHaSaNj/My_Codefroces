358d3799d83c0de95aef6b865605ca77030a2ee9
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-09 23:51:28 +0600

    (A) Cheap Travel.cpp

---
+++ (A) Cheap Travel.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+
+    int c1 = n * a;
+
+
+    int c2= ((n + m - 1) / m) * b;
+
+    int c3 = (n / m) * b + (n % m) * a;
+
+
+    int result = min({c1, c2, c3});
+
+    cout << result << endl;
+    return 0;
+}
