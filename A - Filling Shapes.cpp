ddd95585ad1140bb593faaf16cede3cfd8f7ee2b
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-10 23:16:14 +0600

    A - Filling Shapes.cpp

---
+++ A - Filling Shapes.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using  namespace std;
+int main(){
+
+
+
+long long  n;
+cin>>n;
+if(n%2!=0){
+
+    cout<<0<<endl;}
+    else {
+
+       cout<<fixed<<setprecision(0)<<pow(2,n/2)<<endl;
+    }
+}
