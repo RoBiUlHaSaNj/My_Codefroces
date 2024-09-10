f066a8eb7d442af7aa2551b48452cf8f46880ce4
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-10 23:14:40 +0600

    A - Fancy Fence.cpp

---
+++ A - Fancy Fence.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a;
+        cin >> a;
+
+
+        int n = 360 / (180 - a);
+
+        if (360 % (180 - a) == 0 && n >= 3) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
