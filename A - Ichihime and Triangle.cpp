d26cf63cd4971a4a265d8718e51c5f83b867c291
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-13 22:42:55 +0600

    A - Ichihime and Triangle.cpp

---
+++ A - Ichihime and Triangle.cpp
@@ -0,0 +1,18 @@
+ #include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+//Striver_CP_Sheet_Math_1
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll a, b, c, d, x, y, z;
+        cin >> a >> b >> c >> d;
+
+ cout<<b<<" "<<c<<" "<<c<<endl;
+     
+    }
+
+    return 0;
+}
