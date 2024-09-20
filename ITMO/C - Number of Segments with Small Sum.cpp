470708cabd7a629f785e481c3cc582cbb37ca061
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-20 23:26:42 +0600

    C - Number of Segments with Small Sum.cpp

---
+++ ITMO/C - Number of Segments with Small Sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+typedef long long ll;
+int main()
+{
+
+  ll n,k;
+    cin>>n>>k;
+    vector<ll>a(n);
+   // vector<ll>b(k);
+ll maxl=0;
+    vector<ll>ans;
+    for(int i =0; i<n; i++)
+    {
+        cin>>a[i];
+        //ans.push_back(a[i]);
+    }
+   // for(int i =0; i<k; i++)
+    //{
+       // cin>>b[i];
+        //ans.push_back(b[i]);
+   // }
+   // sort(ans.begin(),ans.end());
+
+    //for(int i =0; i<ans.size(); i++)
+    //{
+        //cout<<ans[i]<<" ";
+   // }
+   //int r=0;
+   int l=0;
+   ll cs=0;
+   for(int r= 0;r<n;r++){cs+=a[r];
+
+   while(cs>k){cs-=a[l];l++;}
+   //int maxl=0;
+   maxl+=r-l+1;
+   //cout<<maxl;//find maximum sum
+   }
+   cout<<maxl<<" ";
+
+}
