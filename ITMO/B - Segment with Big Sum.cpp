4261c000124570c7dd87eab21990c330f91f99e7
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-20 23:27:13 +0600

    B - Segment with Big Sum.cpp

---
+++ ITMO/B - Segment with Big Sum.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    long long s;
+    cin >> n >> s;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    int l = 0, ans = n + 1;
+    long long sum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        sum += v[i];
+        while (sum >= s) {
+            ans = min(ans, i - l + 1);
+            sum -= v[l++];
+        }
+    }
+
+    if (ans > n) {
+        ans = -1;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
