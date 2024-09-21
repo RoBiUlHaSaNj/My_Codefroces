1597e354b821735a4735bce71e33e595d4fea3c5
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-21 23:50:53 +0600

    B - Two Buttons.cpp

---
+++ B - Two Buttons.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int ans = 0;
+
+    while (m > n) {
+        if (m % 2 == 0) {
+            m /= 2; 
+        } else {
+            m++; 
+        }
+      ans++;
+    }
+
+   ans += (n - m); 
+
+    cout << ans << endl;
+
+   
+}
