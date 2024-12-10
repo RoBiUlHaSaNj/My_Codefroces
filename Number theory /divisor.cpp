12cd0973ff1827bfbf34b53227db11db989345ca
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-12-10 15:22:54 +0600

    divisor.cpp

--- Number theory /divisor.cpp
+++ Number theory /divisor.cpp
@@ -29,4 +29,52 @@ int32_t main() {
     divisor(n);
 
 }
+ 
+int main() {
+    long long N;
+    cin >> N;
+    
 
+    vector<long long> f(N +1);
+  long long sum_of_divisors = 1;
+  //count 
+    for (long long i = 1; i <= N; ++i) {
+        for (long long j = i; j <= N; j += i) {
+            f[j]++;
+        }
+    }
+   
+    
+    
+ //range variation 
+    long long result=0;
+    for (long long i = 1; i <= N; ++i) {
+        result+=f[i];//
+    }
+
+    cout << result << endl;
+    return 0;
+}
+/*
+1.find divisor of N=4
+4
+1
+2
+4
+2.find count of divisor number N=4;
+4
+3
+3.find sum,multi,divi of divisor N=4
+4
+1 +2 +4=7
+4.find range no of divisor count
+4
+1=1
+2=2
+3=2
+4=3
+output 8
+
+
+
+*/
